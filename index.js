2803bbf93e950dc20bb29b410cc635894b63345d
Mayank-3e <111231656+Mayank-3e@users.noreply.github.com>
2022-12-22 21:54:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: process.env.FRONTEND}))
 
 // Available Routes
 app.use('/api/auth',require('./routes/auth'))
